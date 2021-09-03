b8ec2a9d2533df171ec57d1db2dcab94e9952f46
Bluzzard <87590083+BluzzardTheWizard@users.noreply.github.com>
2021-09-03 06:39:54 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var sttbutton = document.getElementById("stt");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    sttbutton.style.display = "block";
+  } else {
+    sttbutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+console.log(':)')
